e054f217cc55ed655b56ceebeca0ae1bcf6b959a
Aleksei Faians <af.git@bsrealm.net>
2026-07-30 02:24:20 +0800

    Update ReplySortIndexRecCollection implementation and debugging messages

--- mbtool.pas
+++ mbtool.pas
@@ -107,37 +107,32 @@ begin
 end;
 
 procedure ReplySortIndexRecCollection;
-procedure SortIndexRecCollection;
 var
   I, J, ParentIdx: Longint;
-  IndexRec, Rec2: PIndexRec;
+  Rec1, Rec2: PIndexRec;
 begin
   I := 0;
   while I < IndexRecCollection.Count do
   begin
-    IndexRec := PIndexRec(IndexRecCollection.At(I));
-    if (not IndexRec^.HasTZUTC) and (Length(IndexRec^.ReplyMSGID^) > 0) and (not IsCleanAddress(IndexRec^.ReplyAddress)) then
+    Rec1 := IndexRecCollection.At(I);
+    if (not Rec1^.HasTZUTC) and (Length(Rec1^.ReplyMSGID^) > 0) and (not IsCleanAddress(Rec1^.ReplyAddress)) then
     begin
       ParentIdx := -1;
       for J := 0 to IndexRecCollection.Count - 1 do
       begin
-        Rec2 := PIndexRec(IndexRecCollection.At(J));
-        if Assigned(Rec2^.MSGID) and (Rec2^.MSGID^ = IndexRec^.ReplyMSGID^) and (AddressCompare(Rec2^.FromAddress, IndexRec^.ReplyAddress) = 0) then
+        Rec2 := IndexRecCollection.At(J);
+        if Assigned(Rec2^.MSGID) and (Rec2^.MSGID^ = Rec1^.ReplyMSGID^) and (AddressCompare(Rec2^.FromAddress, Rec1^.ReplyAddress) = 0) then
         begin
           ParentIdx := J;
           break;
         end;
       end;
-
-      if ParentIdx <> -1 then
+      if I < ParentIdx then
       begin
-        if I < ParentIdx then
-        begin
-          IndexRecCollection.AtDelete(I);
-          IndexRecCollection.AtInsert(ParentIdx, IndexRec);
-          WriteLn('[INFO] Message #', IndexRec^.MsgNum, ' sorted after #', PIndexRec(IndexRecCollection.At(ParentIdx - 1))^.MsgNum, ' (missing TZUTC, moved after parent MSGID: ', IndexRec^.ReplyMSGID^, ')');
-          continue;
-        end;
+        IndexRecCollection.AtDelete(I);
+        IndexRecCollection.AtInsert(ParentIdx, Rec1);
+        WriteLn('[INFO] Message #', Rec1^.MsgNum, ' sorted after #', Rec2^.MsgNum, ' because it was missing TZUTC kludge and is a reply to that message.');
+        continue;
       end;
     end;
     Inc(I);
