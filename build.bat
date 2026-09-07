ca0622c3b694786d4038483122e82cb51b5c7f9e
Aleksei Faians <af.git@bsrealm.net>
2026-09-07 14:37:18 +0800

    build: use a for loop in build.bat to build mbtool and recode

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 @echo off
 
-fpc.exe -B -dRELEASE -Flout -FUout -Fud:\prog\pascal\skMHL-avs\sources -Fid:\prog\pascal\skMHL-avs\sources -g -gl mbtool.pas
+for %%i in (mbtool.pas recode.pas) do (
+    fpc.exe -B -dRELEASE -Flout -FUout -Fud:\prog\pascal\skMHL-avs\sources -Fid:\prog\pascal\skMHL-avs\sources -g -gl %%i
+)
