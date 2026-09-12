b901de56d177723f05c72c2147656b91700b4615
Aleksei Faians <af.git@bsrealm.net>
2026-09-12 20:26:26 +0800

    Remove -g -gl compiler options - makes no sense with -dRELEASE

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 
 for %%i in (mbtool.pas recode.pas) do (
-    fpc.exe -B -dRELEASE -Flout -FUout -Fud:\prog\pascal\skMHL-avs\sources -Fid:\prog\pascal\skMHL-avs\sources -g -gl %%i
+    fpc.exe -B -dRELEASE -Flout -FUout -Fud:\prog\pascal\skMHL-avs\sources -Fid:\prog\pascal\skMHL-avs\sources %%i
 )
